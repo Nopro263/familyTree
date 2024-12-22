16b9a7496453186dd46a608f1e7039b35e16fec6
Noah <82201000+Nopro263@users.noreply.github.com>
2024-12-22 16:47:21 +0100

    added tree structure

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-import { connectElements, createElement, initCanvas } from "./canvas.js";
+import { createTree, createNode, addChildren, addRelationship } from "./tree.js";
 
-const canvas = document.querySelector(".mainCanvas");
+const tree = createTree(".mainCanvas");
 
-initCanvas(canvas);
+console.log(createNode(tree, "Noah"));
+console.log(createNode(tree, "Emily"));
 
-const a = createElement(canvas);
+console.log(addRelationship(tree, 0,1));
+
+/*const a = createElement(canvas);
 a.innerHTML = "A";
 const b = createElement(canvas);
 b.innerHTML = "B";
 
 const line_ab = connectElements(canvas, a,b);
-line_ab.innerHTML = "⚭";
+line_ab.innerHTML = "⚭";*/
---
+++ tree.js
@@ -0,0 +1,62 @@
+import { connectElements, createElement, initCanvas } from "./canvas.js";
+
+let nodeId = 0;
+let relationshipId = 0;
+
+export const createTree = (selector) => {
+    const canvas = document.querySelector(selector);
+
+    initCanvas(canvas);
+
+    return {
+        "nodes": [],
+        "relationships": [],
+        "canvas": canvas
+    }
+}
+
+export const createNode = (tree, name) => {
+    const element = createElement(tree.canvas);
+
+    element.innerHTML = name;
+
+    const node = {
+        "name": name,
+        "id": nodeId++,
+        "element": element
+    }
+
+    tree.nodes.push(node);
+    return node;
+}
+
+export const addRelationship = (tree, node1Id, node2Id) => {
+    const node1 = _getNode(tree, node1Id);
+    const node2 = _getNode(tree, node2Id);
+
+    const element = connectElements(tree.canvas, node1.element, node2.element);
+    element.innerHTML = "+";
+
+    const relationship = {
+        "nodes": [node1Id, node2Id],
+        "id": relationshipId++,
+        "children": [],
+        "element": element
+    }
+
+    tree.relationships.push(relationship);
+    return relationship;
+}
+
+export const addChildren = (tree, relationshipId, nodeId) => {
+    const relationship = _getRelationship(tree, relationshipId);
+    relationship.children.push(nodeId);
+}
+
+const _getRelationship = (tree, id) => {
+    return tree.relationships.filter((v) => v.id === id)[0];
+}
+
+const _getNode = (tree, id) => {
+    return tree.nodes.filter((v) => v.id === id)[0];
+}
